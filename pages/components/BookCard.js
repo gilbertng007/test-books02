eb879fd05ffc23d6307c0fc1c3645f4a8b766743
gilbertng007 <mkngmkng@gmail.com>
2024-09-27 00:37:06 +0800

    change

--- pages/components/BookCard.js
+++ pages/components/BookCard.js
@@ -24,6 +24,43 @@
 //   );
 // }
 
+// import React from 'react';
+// import Image from 'next/image';
+
+// export default function BookCard({ book }) {
+//   const handleViewDetails = () => {
+//     if (book?.id) { // 确保 book 对象和 book.id 存在
+//       window.location.href = `/book?id=${book.id}`;
+//     }
+//   };
+
+//   return (
+//     <div className="card h-100">
+//       {book?.cover_image ? (
+//         <Image
+//           src={book.cover_image}
+//           className="card-img-top"
+//           alt={book?.title}
+//           layout="responsive"
+//           sizes="100vw"
+//           style={{ width: '100%', height: 'auto' }}
+//         />
+//       ) : (
+//         <div className="card-img-top" style={{ width: '100%', height: 'auto' }}>
+//           Image not available
+//         </div>
+//       )}
+//       <div className="card-body">
+//         <h5 className="card-title">{book?.title}</h5>
+//         <p className="card-text">{book?.author}</p >
+//         <button onClick={handleViewDetails} className="btn btn-primary" disabled={!book?.id}>
+//           查看详情
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
+
 import React from 'react';
 import Image from 'next/image';
 
@@ -42,8 +79,8 @@ export default function BookCard({ book }) {
           className="card-img-top"
           alt={book?.title}
           layout="responsive"
-          sizes="100vw"
-          style={{ width: '100%', height: 'auto' }}
+          width={500} // 例如，设置一个默认宽度
+          height={700} // 例如，设置一个默认高度
         />
       ) : (
         <div className="card-img-top" style={{ width: '100%', height: 'auto' }}>
