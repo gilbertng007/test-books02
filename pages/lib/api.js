a04d069925a2f3ff47f44730380fdfb8d7169ff5
gilbertng007 <mkngmkng@gmail.com>
2024-09-29 01:17:08 +0800

    change api link

--- pages/lib/api.js
+++ pages/lib/api.js
@@ -14,8 +14,8 @@ export default function Ac() {
 export const fetchBooks = async () => {
   // const response = await fetch('https://freetestapi.com/api/v1/books');
   // const response = await fetch('https://my-json-server.typicode.com/gilbertng007/bookstore-02/books');
-  // const response = await fetch('http://localhost:3000/api/books');
-  const response = await fetch('http://localhost:4000/bookstore');
+  const response = await fetch('http://localhost:3000/api/books');
+  // const response = await fetch('http://localhost:4000/bookstore');
   if (!response.ok) {
     throw new Error(`HTTP error! Status: ${response.status}`);
   }
@@ -27,8 +27,8 @@ export const fetchBooks = async () => {
 export const fetchBookById = async id => {
   // const response = await fetch(`https://freetestapi.com/api/v1/books/${id}`);
   // const response = await fetch(`https://my-json-server.typicode.com/gilbertng007/bookstore-02/books`);
-  // const response = await fetch(`http://localhost:3000/api/books/${id}`);
-  const response = await fetch(`http://localhost:4000/bookstore/${id}`);
+  const response = await fetch(`http://localhost:3000/api/books/${id}`);
+  // const response = await fetch(`http://localhost:4000/bookstore/${id}`);
 
   if (!response.ok) {
     throw new Error(`HTTP error! Status: ${response.status}`);
